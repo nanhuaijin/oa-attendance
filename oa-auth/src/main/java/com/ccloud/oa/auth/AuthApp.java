979056f5533b1e6e5adb6deb4162d4e9e52bcee5
breeze <nanhuaijin77@gmail.com>
2020-08-14 15:15:12 +0800

    feat：认证服务搭建

---
+++ oa-auth/src/main/java/com/ccloud/oa/auth/AuthApp.java
@@ -0,0 +1,16 @@
+package com.ccloud.oa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : breeze
+ * @date : 2020/8/14
+ * @description : 启动
+ */
+@SpringBootApplication
+public class AuthApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApp.class, args);
+    }
+}
---
+++ oa-auth/src/main/java/com/ccloud/oa/auth/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.ccloud.oa.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : breeze
+ * @date : 2020/8/14
+ * @description : 登录认证接口
+ */
+@RestController
+public class AuthController {
+}
